fb02e9003603a3ff90c1821f04fd1bbac35e4945
JAMSHEED <jamsheed.x48@gmail.com>
2026-04-17 17:07:17 +0530

    removed secret key

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,11 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-beubgy7t(ct-5#-eq6j_z1ql2v5as@+vm0%_7z=pre)g)irc#2'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
